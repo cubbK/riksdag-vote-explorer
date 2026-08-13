7c5b69308ecfc9afb571aaf48a70916024daf58d
Dan <yo@gmail.om>
2026-08-13 22:56:59 +0200

    read in duckdb all datasets

--- exploration.py
+++ exploration.py
@@ -5,7 +5,7 @@ con = duckdb.connect()
 
 # %%
 # %%
-df = con.sql("""
+df_betankade = con.sql("""
     SELECT
         dokumentstatus.dokument.dok_id AS parent_dok_id,
         dokumentstatus.dokument.titel AS parent_titel,
@@ -16,7 +16,81 @@ df = con.sql("""
     ORDER BY parent_dok_id
 """).pl()
 
-df
+df_betankade
 
+
+# %%
+
+df_motioner = con.sql("""
+    WITH src AS (
+        SELECT
+            CASE json_type(to_json(dokumentstatus.dokreferens.referens))
+                WHEN 'ARRAY' THEN CAST(to_json(dokumentstatus.dokreferens.referens) AS JSON[])
+                ELSE [CAST(to_json(dokumentstatus.dokreferens.referens) AS JSON)]
+            END AS refs
+        FROM read_json('datasets/json/motioner/*.json', auto_detect=true, ignore_errors=true)
+    )
+    SELECT
+        ref->>'referenstyp'          AS referenstyp,
+        ref->>'uppgift'              AS uppgift,
+        ref->>'ref_dok_id'           AS ref_dok_id,
+        ref->>'ref_dok_typ'          AS ref_dok_typ,
+        ref->>'ref_dok_rm'           AS ref_dok_rm,
+        ref->>'ref_dok_bet'          AS ref_dok_bet,
+        ref->>'ref_dok_titel'        AS ref_dok_titel,
+        ref->>'ref_dok_subtitel'     AS ref_dok_subtitel,
+        ref->>'ref_dok_subtyp'       AS ref_dok_subtyp,
+        ref->>'ref_dok_dokumentnamn' AS ref_dok_dokumentnamn
+    FROM src,
+    UNNEST(refs) AS t(ref)
+    
+""").pl()
+
+df_motioner
+
+
+# %%
+
+
+df_propositioner = con.sql("""
+    WITH src AS (
+        SELECT
+            CASE json_type(to_json(dokumentstatus.dokreferens.referens))
+                WHEN 'ARRAY' THEN CAST(to_json(dokumentstatus.dokreferens.referens) AS JSON[])
+                ELSE [CAST(to_json(dokumentstatus.dokreferens.referens) AS JSON)]
+            END AS refs
+        FROM read_json('datasets/json/propositioner/*.json', auto_detect=true, ignore_errors=true, maximum_object_size=104857600)
+    )
+    SELECT
+        ref->>'referenstyp'          AS referenstyp,
+        ref->>'uppgift'              AS uppgift,
+        ref->>'ref_dok_id'           AS ref_dok_id,
+        ref->>'ref_dok_typ'          AS ref_dok_typ,
+        ref->>'ref_dok_rm'           AS ref_dok_rm,
+        ref->>'ref_dok_bet'          AS ref_dok_bet,
+        ref->>'ref_dok_titel'        AS ref_dok_titel,
+        ref->>'ref_dok_subtitel'     AS ref_dok_subtitel,
+        ref->>'ref_dok_subtyp'       AS ref_dok_subtyp,
+        ref->>'ref_dok_dokumentnamn' AS ref_dok_dokumentnamn
+    FROM src,
+    UNNEST(refs) AS t(ref)
+    
+""").pl()
+
+df_propositioner
+
+# %%
+
+
+df_personlista = con.sql("""
+    SELECT p.intressent_id, p.tilltalsnamn, p.efternamn, p.parti, p.status, 
+    p.bild_url_192
+FROM read_json('datasets/json/personlista.json', auto_detect=true, ignore_errors=true),
+UNNEST(personlista.person) AS t(p)
+WHERE p.status LIKE 'Tjänstgörande%'
+ORDER BY p.parti, p.efternamn
+""").pl()
+
+df_personlista
 
 # %%
